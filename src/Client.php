599b57f57bd5feac82e6b6a28f5408b905d9a6c3
Younès El Biache <younes.elbiache@gmail.com>
2014-05-15 19:57:07 +0200

    optional params; refactoring

--- src/Client.php
+++ src/Client.php
@@ -12,19 +12,19 @@ class Client
 	public function __construct($accessToken)
 	{
 		$this->httpClient = new \GuzzleHttp\Client(array(
-		    'base_url' => array($this->baseUrl, array('version' => null)),
-		    'defaults' => array(
-		        'headers' => array(
-		        	'Authorization' => 'Bearer '.$accessToken
-		        ),
-		    )
+			'base_url' => array($this->baseUrl, array('version' => null)),
+			'defaults' => array(
+				'headers' => array(
+					'Authorization' => 'Bearer '.$accessToken
+				),
+			)
 		));
 	}
 
 	/**
 	 * Fetch All Snips Created By User or Get A Specific Snip
 	 * 
-	 * @param  String $slug    The slug is the three-character identifier that is included in the snip's URL
+	 * @param  String $slug	The slug is the three-character identifier that is included in the snip's URL
 	 * @return Array|Guzzle\Http\Message\Response Guzzle response body|object
 	 */
 	public function fetch($slug = null, $body = true)
@@ -37,44 +37,57 @@ class Client
 	/**
 	 * Create a new snip
 	 * 
-	 * @param  String $url     The url that you would like to shorten
+	 * @param  String $url	 The url that you would like to shorten
 	 * @param  String $message The text of the message that you would like to display in the snip. HTML messages are not supported
 	 * @return Array|Guzzle\Http\Message\Response Guzzle response body|object
 	 */
-	public function create($url, $message, $body = true)
+	public function create($url, $message, $optional = array(), $body = true)
 	{
-		return $this->output(
- 			$this->httpClient->post('snips/', array(
-				'body' => array( 
-					'url'     => $url,
-					'message' => $message,
-				),
-			)
-		), $body);		
+		return $this->createOrEdit(null, $url, $message, $optional, $body);
 	}
 
 	/**
 	 * Edit a snip
 	 *
-	 * @param  String $slug    The slug is the three-character identifier that is included in the snip's URL
-	 * @param  String $url     The url that you would like to shorten
+	 * @param  String $slug	The slug is the three-character identifier that is included in the snip's URL
+	 * @param  String $url	 The url that you would like to shorten
 	 * @param  String $message The text of the message that you would like to display in the snip. HTML messages are not supported
+	 * @param  Array $optional Additional parameters
 	 * @return Array|Guzzle\Http\Message\Response Guzzle response body|object 
 	 */
-	public function edit($slug, $url, $message, $body = true)
+	public function edit($slug, $url, $message, $optional = array(), $body = true)
 	{
-	 	return $this->output(
-	 		$this->httpClient->post(sprintf('snips/%s/', $slug), array(
-				'body' => array( 
-					'url'     => $url,
+		return $this->createOrEdit($slug, $url, $message, $optional, $body);
+	}
+
+
+	/**
+	 * Edit a snip
+	 *
+	 * @param  String $slug
+	 * @param  String $url
+	 * @param  String $message
+	 * @param  Array $optional
+	 * @return Array|Guzzle\Http\Message\Response Guzzle response body|object 
+	 */
+	protected function createOrEdit($slug = null, $url, $message, $optional = array(), $body = true)
+	{
+		$url = $slug ? sprintf('snips/%s/', $slug) : 'snips/';
+
+		return $this->output(
+			$this->httpClient->post($url, array(
+				'headers' => array('content-type' => 'application/json'),
+				'body' => json_encode(array_merge($optional, array(
+					'url' => $url,
 					'message' => $message,
-				)
+				))),
 			)
-		), $body);	
+		), $body);
 	}
 
 	/**
-	 * [output description]
+	 * Output
+	 * 
 	 * @param  Guzzle\Http\Message\Response $response Guzzle response object 
 	 * @param  boolean $body 
 	 * @return Array|Guzzle\Http\Message\Response Guzzle response body|object 
